acc7cce4dc2400575c3ae7fad4b9f07d518a6e89
ambernick <ambernich2@yahoo.com>
2020-12-17 11:39:41 -0500

    add routes and redirects

--- routes/auth.js
+++ routes/auth.js
@@ -2,21 +2,21 @@ const router = require("express").Router();
 const passport = require("passport");
 
 router.get(
-    "/google",
-    passport.authenticate("google", { scope: ["profile", "email"] })
-  );
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
-  router.get(
-    "/google/oauth2callback",
-    passport.authenticate("google", {
-      successRedirect: "/students",
-      failureRedirect: "/students",
-    })
-  );
+router.get(
+  "/google/oauth2callback",
+  passport.authenticate("google", {
+    successRedirect: "/users",
+    failureRedirect: "/auth/google",
+  })
+);
 
-  router.get("/logout", function (req, res) {
-    req.logout();
-    res.redirect("/students");
-  });
+router.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
+});
 
-  module.exports = router;
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const router = require("express").Router();
+const usersCtrl = require("../controllers/users");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get("/", isLoggedIn, usersCtrl.index);
+// router.get("/profile", isLoggedIn, usersCtrl.showProfile)
+// router.put("/profile", isLoggedIn, usersCtrl.update)
+// router.get("/:id", isLoggedIn, usersCtrl.show)
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/auth/google");
+}
 
 module.exports = router;
