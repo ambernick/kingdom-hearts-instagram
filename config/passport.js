407555db9652e3a967f4f6b5b565c731868d67b8
ambernick <ambernich2@yahoo.com>
2020-12-17 11:42:36 -0500

    edit passport.js and fix google auth

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/user");
 
 passport.use(
     new GoogleStrategy(
