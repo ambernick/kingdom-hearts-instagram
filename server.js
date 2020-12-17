1fcabd7c4f906cf86688ce516dbde7807edf59a7
ambernick <ambernich2@yahoo.com>
2020-12-17 11:38:32 -0500

    add routes

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const methodOverride = require("method-override");
 const session = require("express-session");
 const passport = require("passport");
+const methodOverride = require("method-override");
 
 require("dotenv").config();
+
 require("./config/database");
+
 require("./config/passport");
 
 const indexRouter = require('./routes/index');
