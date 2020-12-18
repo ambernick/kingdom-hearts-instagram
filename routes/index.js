2e04d879e9b6fe5d4728d25d96e2eab0bf0b30a7
ambernick <ambernich2@yahoo.com>
2020-12-18 13:46:56 -0500

    add Post schema

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const usersCtrl = require("../controllers/users");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Kingdom Hearts' });
-});
+router.get("/", isLoggedIn, usersCtrl.index);
 
+//  {
+//   res.render('index', { title: 'Kingdom Hearts', user: req.user ? req.user : null });
+//   isLoggedIn()
+// });
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.render('index', { title: 'Kingdom Hearts', user: req.user ? req.user : null });
+  res.redirect("/auth/google");
+}
 module.exports = router;
---
+++ routes/posts.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema(
+    {
+        name: String,
+        email: String,
+        avatar: String,
+        googleId: String,
+        Likes: Number,
+        Caption: String,
+        Comments: String,
+        
+      },
+      {
+        timestamps: true,
+      }
+    );
+    
+    module.exports = mongoose.model("Post", postSchema);
+)
