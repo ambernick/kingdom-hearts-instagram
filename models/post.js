8e0e58f4e6162cf8ef70346f074f4b192d0aaea5
ambernick <ambernich2@yahoo.com>
2020-12-18 13:50:04 -0500

    add Post schema

---
+++ models/post.js
@@ -0,0 +1,19 @@
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema(
+    {
+        name: String,
+        email: String,
+        avatar: String,
+        googleId: String,
+        Likes: Number,
+        Caption: String,
+        Comments: String,
+        
+      },
+      {
+        timestamps: true,
+      }
+    );
+    
+    module.exports = mongoose.model("Post", postSchema);
